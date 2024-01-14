66b20932fd8fd9294904eccc438afcb4332d3867
Onyedikachi Nwanguma <119358391+HexTech19@users.noreply.github.com>
2024-01-14 01:52:02 +0100

    still on it

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-function clicked() {
-  let clickedButton = document.querySelectorAll('.button');
-  console.log(clickedButton[3].innerText);
+const wordDisplay = document.querySelector('.word-display');
+const keyboard = document.querySelector('.keyboard');
+
+let currentWord;
+const getRandomWord = () => {
+  const {word, hint} = words[Math.floor(Math.random() * words.length)];
+  console.log(word)
+  currentWord = word;
+  document.querySelector('.hint-text').innerText = hint;
+  wordDisplay.innerHTML = word.split('').map( () => ` <li class="letter"></li>`).join('');
 }
 
-clicked();
+
+const inGame = (button, clickedLetter) => {
+  if (currentWord.includes(clickedLetter)) {
+    console.log(clickedLetter, 'is in the word');
+  } else {
+    console.log(clickedLetter, 'is not in the word');
+  }
+}
+
+//Creating buttons and adding Event listeners
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement('button');
+  button.innerText = String.fromCharCode(i)
+  keyboard.appendChild(button);
+  button.addEventListener('click', e => inGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
---
+++ words.js
@@ -0,0 +1,22 @@
+const words = [
+  {
+    word: 'Arsenal',
+    hint: 'The best football club in England'
+  },
+  {
+    word: 'TenHaag',
+    hint: 'The best Manager club in England'
+  },
+  {
+    word: 'Chelsea',
+    hint: 'Yahoo Boys'
+  },
+  {
+    word: 'Aguerooo',
+    hint: 'Best moment in PL history'
+  },
+  {
+    word: 'Hazard',
+    hint: 'Danger'
+  },
+];
