e76bdcfdb760581c44b52d5ad36b9af34a3e893f
Onyedikachi Nwanguma <119358391+HexTech19@users.noreply.github.com>
2024-01-15 19:55:30 +0100

    Update words.js

--- words.js
+++ words.js
@@ -4,19 +4,79 @@ const words = [
     hint: 'The best football club in England'
   },
   {
-    word: 'tenhaag',
-    hint: 'The best Manager club in England'
+    word: 'dominica',
+    hint: 'A Country the Caribbean'
   },
   {
-    word: 'chelsea',
-    hint: 'Yahoo Boys'
+    word: 'georgia',
+    hint: 'A Country in Europe'
   },
   {
-    word: 'aguerooo',
-    hint: 'Best moment in PL history'
+    word: 'comoros',
+    hint: 'A Country in Africa'
+  },
+  {
+    word: 'djibouti',
+    hint: 'A Country in Africa'
+  },
+  {
+    word: 'evalution',
+    hint: 'English Word'
+  },
+  {
+    word: 'recitation',
+    hint: 'English Word'
+  },
+  {
+    word: 'anonymous',
+    hint: 'English Word'
+  },
+  {
+    word: 'whizzing',
+    hint: 'English Word'
+  },
+  {
+    word: 'fallacy',
+    hint: 'English Word'
   },
   {
     word: 'hazard',
-    hint: 'Danger'
+    hint: 'English Word'
   },
+  {
+    word: 'amadeus',
+    hint: 'Great movie'
+  },
+  {
+    word: 'evalution',
+    hint: 'English Word'
+  },
+  {
+    word: 'jukebox',
+    hint: 'Music'
+  },
+  {
+    word: 'messi',
+    hint: 'G.O.A.T'
+  },
+  {
+    word: 'hextech',
+    hint: 'Maker of this game'
+  },
+  {
+    word: 'eilish',
+    hint: 'Female Artiste'
+  },
+  {
+    word: 'simisola',
+    hint: 'Female Artiste'
+  },
+  {
+    word: 'mnemonic',
+    hint: 'English Word'
+  },
+  {
+    word: 'axiom',
+    hint: 'English Word'
+  }
 ];
